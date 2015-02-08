b4098f2710c6a804c9d8f8dea52f44b699f4ec22
jdfu <jack.fu@gmx.de>
2015-02-09 00:17:31 +0100

    First Commit

---
+++ README.m
@@ -0,0 +1,14 @@
+%% test matlab file
+clc
+clear all
+close all
+format long g
+
+numbersFrom1To10=1:10
+
+%% a parabola
+
+figure;
+plot(numberFrom1To10,numbersFrom1To10.^2)
+xlabel('x-axis')
+ylabel('y-axis')
