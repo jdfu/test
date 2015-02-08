9218b413bcd523fa19ab612ed7d3cb8bd17593a8
jdfu <jack.fu@gmx.de>
2015-02-09 00:35:13 +0100

    second commit - added password and changed README

--- README.m
+++ README.m
@@ -4,11 +4,21 @@ clear all
 close all
 format long g
 
-numbersFrom1To10=1:10
+numbersFrom1To10=1:10;
 
 %% a parabola
 
 figure;
 plot(numberFrom1To10,numbersFrom1To10.^2)
+xlabel('x-axis')
+ylabel('y-axis')
+
+%% now calculate any parabola given a vector input
+
+x_squared = @(x) x.^2;
+
+% plot an example
+figure;
+plot(-10:10,x_squared(-10:10))
 xlabel('x-axis')
 ylabel('y-axis')
